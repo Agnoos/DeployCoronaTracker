193b4fd4d2af41fbdaa45198c91c952dde34e9a1
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-07-31 18:26:07 -0300

    first commit

---
+++ precache-manifest.aecb669d9331691c71f66c4870e3c58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a6d6e47d4591567da2de5652d8c918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37992781f90c21de2ef",
+    "url": "/static/css/main.dcb3d506.chunk.css"
+  },
+  {
+    "revision": "b558893b47c07b0a7dcb",
+    "url": "/static/js/2.452afc84.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.452afc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37992781f90c21de2ef",
+    "url": "/static/js/main.811006e5.chunk.js"
+  },
+  {
+    "revision": "2ed9ba355c00b4854a21",
+    "url": "/static/js/runtime-main.3410f2ca.js"
+  }
+]);
